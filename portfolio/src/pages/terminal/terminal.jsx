0d85ad94eb256804da4edb7007223ad85044cca2
abhay <abhayrajsinghrana123@gmail.com>
2024-08-15 16:30:19 +0530

    updated second page

--- portfolio/src/pages/terminal/terminal.jsx
+++ portfolio/src/pages/terminal/terminal.jsx
@@ -1,10 +1,225 @@
-import React from 'react'
-
+import React from "react";
+import { ReactTyped } from "react-typed";
+import "../terminal/terminal.css";
 
 export default function Terminal() {
   return (
     <div>
-         <h1>Welcome to the Home Screen!</h1>
+      <div className="container mx-auto bg-black px-10">
+
+      <h2>
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+
+
+
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+
+
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        To run command as administrator (user: 'root'), use "sudo command". See
+        man sudo root for detail.
+        
+      
+
+
+
+
+      </h2>
+      </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+
+      {/* <ReactTyped strings={["Here you can find anything"]} typeSpeed={40} />
+      <br /> */}
+
+      {/* <ReactTyped
+        strings={[
+          "Search for products",
+          "Search for categories",
+          "Search for brands",
+        ]}
+        typeSpeed={40}
+        backSpeed={50}
+        attr="placeholder"
+        loop
+      >
+        <input type="text" />
+      </ReactTyped> */}
     </div>
-  )
+  );
 }
