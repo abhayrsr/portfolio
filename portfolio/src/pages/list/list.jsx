4d41f3276450299be3147f8248203cdecc929e5f
abhay <abhayrajsinghrana123@gmail.com>
2024-11-17 15:51:27 +0530

    feat: grid style

--- portfolio/src/pages/list/list.jsx
+++ portfolio/src/pages/list/list.jsx
@@ -6,10 +6,10 @@ export default function List(){
     return(
         <div class="md:container md:mx-auto size-full">
          <div class="absolute left 0 grid mobile:grid-cols-6 laptop:grid-cols-4 gap-4">
-          <p >experience.txt </p>
+          <p>experience.txt </p>
           <p>skills.txt </p>
-          <p >projects.txt </p>
-          <p >contact.txt </p>
+          <p>projects.txt </p>
+          <p>contact.txt </p>
          </div>
          <br />
          <Path />
