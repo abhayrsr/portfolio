c49501a753a54bddbea4a83c4ec41725342e426f
abhay <abhayrajsinghrana123@gmail.com>
2024-11-17 14:43:37 +0530

    feat: styled terminal

--- portfolio/src/pages/list/list.jsx
+++ portfolio/src/pages/list/list.jsx
@@ -5,7 +5,7 @@ export default function List(){
 
     return(
         <div class="absolute container size-full">
-         <p class="absolute left 0">
+         <p class="absolute left 0 inline-grid grid-cols-4">
           <span class="mr-8">experience.txt </span>
           <span class="mr-8">skills.txt </span>
           <span class="mr-8">projects.txt </span>
