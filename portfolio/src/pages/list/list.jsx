f057e30cd6b8a72dfd86e59ba3baaf3242658ab6
abhay <abhayrajsinghrana123@gmail.com>
2024-11-17 14:46:50 +0530

    feat: grid style

--- portfolio/src/pages/list/list.jsx
+++ portfolio/src/pages/list/list.jsx
@@ -4,8 +4,8 @@ import Path from "../../component/path"
 export default function List(){
 
     return(
-        <div class="absolute container size-full">
-         <p class="absolute left 0 inline-grid grid-cols-4">
+        <div class="absolute md:container md:mx-auto size-full">
+         <p class="grid grid-cols-4 gap-4 flex">
           <span class="mr-8">experience.txt </span>
           <span class="mr-8">skills.txt </span>
           <span class="mr-8">projects.txt </span>
