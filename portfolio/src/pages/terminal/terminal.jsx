7651ce7c26a24cbcb6e55d04e0056833eb92b287
abhay <abhayrajsinghrana123@gmail.com>
2024-11-17 14:22:13 +0530

    feat: styled terminal

--- portfolio/src/pages/terminal/terminal.jsx
+++ portfolio/src/pages/terminal/terminal.jsx
@@ -219,7 +219,7 @@ export default function Terminal({toggle}) {
 
   return (
     <div>
-      <div className="terminal relative size-full">
+      <div className="terminal relative md:container md:mx-auto size-full">
         <div className="terminal-content absolute left 0 font-ubuntu text-white mobile:text-xs tablets:text-xl laptop:text-2xl size-full">
           {/* <p className="absolute left 0">
             To run command as administrator (user: 'root'), use "sudo command".
