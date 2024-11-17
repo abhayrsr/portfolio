0aa25ee827fa43abb56461be1192ce6b61f18d0b
abhay <abhayrajsinghrana123@gmail.com>
2024-11-17 12:11:51 +0530

    feat: break-words

--- portfolio/src/pages/terminal/terminal.jsx
+++ portfolio/src/pages/terminal/terminal.jsx
@@ -226,10 +226,10 @@ export default function Terminal({toggle}) {
             See man sudo root for detail.
           </p>
           <br /> */}
-          <p className="absolute left 0 mobile:break-words">
+          <p className="absolute left 0 ">
             {" "}
             <span class="text-[#24F75A]">abhay@abhay-theDev:</span> ~${" "}
-            <span className={hideCursor ? "cursor-hidden" : ""}>
+            <span className={hideCursor ? "cursor-hidden" : "mobile:break-words"}>
               <ReactTyped
                 strings={["sudo apt install portfolio"]}
                 typeSpeed={100}
