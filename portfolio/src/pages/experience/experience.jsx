3447cab603977a69ae07229ea4445bb5b5313691
abhay <abhayrajsinghrana123@gmail.com>
2024-11-18 15:25:20 +0530

    feat: aligned texts

--- portfolio/src/pages/experience/experience.jsx
+++ portfolio/src/pages/experience/experience.jsx
@@ -10,30 +10,30 @@ export default function Experience() {
           System Engineer, <br></br>Infosys <br></br>{" "}
         </p>
         <ul class="list-inside list-none col-span-2 mobile:text-center">
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
             Engineered and deployed a responsive support portal for Daimler’s
             team using React and Node.js, enhancing user experience and
             resulting in a 25% improvement in issue resolution time.{" "}
           </li>
           <br></br>
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
             Designed and implemented RESTful APIs with Node.js, leading to a 30%
             increase in data processing efficiency between the frontend and
             backend.
           </li>
           <br></br>
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
             Integrated Azure services, optimizing cloud resource utilization and
             achieving a 20% reduction in application latency
           </li>
           <br></br>
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
             Led the migration of a legacy system to a modern architecture using
             MySQL, reducing data retrieval times by 35% and improving overall
             system performance.
           </li>
           <br></br>
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
             Adopted Test-Driven Development (TDD) practices to ensure the
             delivery of robust JavaScript code, resulting in a 20% decrease in
             post-deployment defects.
@@ -46,7 +46,7 @@ export default function Experience() {
         <div class="grid mobile:grid-cols-1 gap-2  laptop:grid-cols-4 gap-4 flex">
           <p class="mobile:text-xl tablet:text-2xl laptop:text-3xl">Copywriter & Personal Development,  <br></br> Freelancer</p>
           <ul class="list-inside list-none col-span-2 mobile:text-center">
-            <li class="text-left">
+            <li class="mobile:text-center laptop:text-left">
               Successfully crafted high-conversion landing and product pages for
               clients, achieving over a 40% increase in user sign-ups and sales,
               while also engaging in defense exam preparation that instilled
--- portfolio/src/pages/projects/projects.jsx
+++ portfolio/src/pages/projects/projects.jsx
@@ -10,13 +10,13 @@ export default function Projects() {
           Online Payment App
         </p>
         <ul class="list-inside list-none col-span-2">
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
            Developed a secure online payment service similar to PhonePe using Node.js and MySQL, 
            implementing robust authentication and encryption protocols to ensure transaction safety and 
            protect sensitive user data. {" "}
           </li>
           <br></br>
-          <li class="text-left">
+          <li class="mobile:text-center laptop:text-left">
            Engineered a scalable backend architecture that efficiently handles high transaction volumes, utilizing 
            RESTful APIs for seamless integration with frontend applications and optimizing database queries in 
            MySQL to enhance performance and response times. 
@@ -29,13 +29,13 @@ export default function Projects() {
         <div class="grid mobile:grid-cols-1 gap-2  laptop:grid-cols-4 gap-4 flex">
           <p class="mobile:text-xl tablet:text-2xl laptop:text-3xl">Food Delivery App</p>
           <ul class="list-inside list-none col-span-2">
-            <li class="text-left">
+            <li class="mobile:text-center laptop:text-left">
             Developed a feature-rich food delivery application akin to Zomato using React, JavaScript, and 
             Bootstrap, implementing intuitive user interfaces that facilitate seamless browsing, ordering, and 
             payment processes, enhancing the overall user experience. 
             </li>
             <br />
-            <li class="text-left">
+            <li class="mobile:text-center laptop:text-left">
             Integrated RESTful APIs to connect with backend services for real-time order tracking, restaurant 
             listings, and user reviews, while utilizing responsive design principles to ensure optimal functionality 
             across various devices and screen sizes.
